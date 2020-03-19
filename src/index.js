b639dcdb37b9df1790429871567ba03209dca5f1
SirArtie <axel_art_Bautista@outlook.com>
2020-03-19 11:23:40 -0600

    Header y Footer plantilla

--- src/index.js
+++ src/index.js
@@ -1,4 +1,11 @@
 // import './src/public/styles/css'
+import  './public/styles/css/header.css';
+import './public/styles/css/footer.css';
+//document.body.innerHTML = 
+//'<h1>Esto ya no es una prueba cracks</h1>'
 
-document.getElementsByClassName('content-body').innerHTML = 
-''
+import Header from './public/templates/Header';
+import Footer from './public/templates/Footer';
+
+document.getElementById("H").innerHTML = Header();
+document.getElementById('F').innerHTML = Footer();
---
+++ src/public/templates/Header.js
@@ -0,0 +1,16 @@
+const Header = () => {
+    const view =
+        `
+        <header>
+            <a href="">Dev-Games</a>
+                <div class='rigth'>
+                    <a href=""> Registrate </a>            
+                    <a href=""> Inicia Sesion </a>
+                    <a href="#"> Contactanos </a>
+                </div>
+        </header>
+        `;  
+        return view;
+};
+
+export default Header;
